73d6a76dc4d157674330282f428fc50ad1ede6bc
Adrienne Breef-Pilz <68744051+abreefpilz@users.noreply.github.com>
2025-03-27 12:20:30 -0400

    updated the function name

--- scripts/run_da_googlevt.R
+++ scripts/run_da_googlevt.R
@@ -6,7 +6,7 @@ pacman::p_load(lubridate,tidyverse, googledrive, readxl, gsheet)
 
 source("https://raw.githubusercontent.com/abreefpilz/try_gdrive_github/refs/heads/main/scripts/download_googlevt.R")
 
-download_google(
+download_googlevt(
   directory = "datavt/",
   gdrive = T, # Are the files on Google Drive. True or False
   gshared_drive = as_id("1JsoCQMDUQH5xgkcGzmBG6O1uWJpmbKSm"),
