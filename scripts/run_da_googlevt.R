c9625311203f7597b7c8784bc23a08e9e4ddf407
abreefpilz@vt.edu <abreefpilz@vt.edu>
2025-03-27 12:06:31 -0400

    try with VT email

---
+++ scripts/run_da_googlevt.R
@@ -0,0 +1,14 @@
+# Run da function
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(lubridate,tidyverse, googledrive, readxl, gsheet)
+
+
+source("https://raw.githubusercontent.com/abreefpilz/try_gdrive_github/refs/heads/main/scripts/download_googlevt.R")
+
+download_google(
+  directory = "datavt/",
+  gdrive = T, # Are the files on Google Drive. True or False
+  gshared_drive = as_id("1JsoCQMDUQH5xgkcGzmBG6O1uWJpmbKSm"),
+  output_file = "test_gdrivevt.csv"
+)
